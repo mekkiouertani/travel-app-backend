a965890926af3cc2a921a6937fa67efa9d843887
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2024-07-30 22:28:32 +0200

    Initial commit

---
+++ src/main/java/com/example/travel_app_backend/TravelAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.travel_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/travel_app_backend/TravelAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travel_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
